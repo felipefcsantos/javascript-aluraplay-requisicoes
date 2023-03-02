e6cb27f4cb7406b6b8e618ec00359ed80e187373
Felipe Santos <felipesantos_fcs98@hotmail.com>
2023-03-02 16:05:06 -0300

    first commit

---
+++ js/buscaVideo.js
@@ -0,0 +1,24 @@
+import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscarVideo(evento) {
+    evento.preventDefault();
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+    const busca = await conectaApi.buscaVideos(dadosDePesquisa);
+    const lista = document.querySelector("[data-lista]")
+
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild)
+    }
+
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+
+    if (busca.length == 0) {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não foram encontrados vídeos com o termo "${dadosDePesquisa}."</h2>`
+    }
+}
+
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]")
+
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
